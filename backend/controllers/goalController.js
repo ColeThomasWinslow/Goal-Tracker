d7105a984d6e39e3aa9ae855e0f6bae4d923a853
Cole Winslow <colethomaswinslow7@gmail.com>
2022-03-04 12:56:32 -0700

    Added Mongo Db and C.R.U.D

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+
 // Get Goals
 // GET /api/goals
 // Auth Required
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // Set Goals
@@ -15,21 +18,39 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please Add a text field");
   }
-  res.status(200).json({ message: "Set Goals" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 
 // Updated Goals
 // PUT /api/goals/:id
 // Auth Required
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Goals ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal Not Found");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 
 // Delete Goals
 // DELETE /api/goals/:id
 // Auth Required
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal Not Found");
+  }
+  await goal.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
